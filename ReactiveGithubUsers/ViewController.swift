66f6ed4454fe480b9022960fbc676306d95348e0
Bogdan Manshilin <skimphiter@gmail.com>
2018-01-29 03:34:20 +0200

    add: refresh button

--- ReactiveGithubUsers/ViewController.swift
+++ ReactiveGithubUsers/ViewController.swift
@@ -15,10 +15,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Initial load
+        let button = UIButton.rx.init(UIButton())
+        button.base.setTitleColor(.red, for: .normal)
+        button.base.setTitle("ХУЙ", for: .normal)
+        button.base.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button.base)
+        NSLayoutConstraint.activate([
+            button.base.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.base.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
         
-        let requestStream = Observable.just("https://api.github.com/users")
+        // All request
         
+        let requestStream = button.tap.startWith(())
+            .map {"https://api.github.com/users?since=\(arc4random_uniform(500))"}
+        
+        // Response
         
         let responseStream = requestStream.flatMap { url -> Observable<Any> in
             return URLSession.shared.rx.json(url: URL(string: url)!)
